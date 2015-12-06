fe5587201c35fb83da603a8f8bdd80bb3fc03aaf
vikatory <vikatory@sina.com>
2015-12-06 22:11:24 +0800

    commit

--- private/hejia/packages.el
+++ private/hejia/packages.el
@@ -14,7 +14,24 @@
 ;; which require an initialization must be listed explicitly in the list.
 (setq hejia-packages
     '(
-      ;; package names go here
+      mwe-log-commands
+      swiper
+      counsel
+      projectile
+      find-file-in-project
+      helm-github-stars
+      magit
+      helm-ls-git
+      beacon
+      evil-vimish-fold
+      avy
+      multiple-cursors
+
+
+
+
+
+      ;; elfeed
       ))
 
 ;; List of packages to exclude.
@@ -25,7 +42,335 @@
 ;; (defun hejia/init-my-package ()
 ;;   "Initialize my package"
 ;;   )
-;;
+
+;; 显示操作记录
+(defun hejia/init-mwe-log-commands ()
+  (use-package mwe-log-commands
+    :init
+    (progn
+      (spacemacs/declare-prefix "ol" "command log")
+      (evil-leader/set-key
+        "oll" 'mwe:log-keyboard-commands
+        "olf" 'mwe:open-command-log-buffer))))
+
+
+(defun hejia/init-swiper ()
+  "Initialize my package"
+  (use-package swiper
+    :init
+    (progn
+      (setq ivy-display-style 'fancy)
+
+      ;; http://oremacs.com/2015/04/16/ivy-mode/
+      (ivy-mode 1)
+      ;; (setq magit-completing-read-function 'ivy-completing-read)
+
+      ;; http://oremacs.com/2015/04/19/git-grep-ivy/
+      (defun counsel-git-grep-function (string &optional _pred &rest _u)
+        "Grep in the current git repository for STRING."
+        (split-string
+         (shell-command-to-string
+          (format
+           "git --no-pager grep --full-name -n --no-color -i -e \"%s\""
+           string))
+         "\n"
+         t))
+
+      (defun counsel-git-grep ()
+        "Grep for a string in the current git repository."
+        (interactive)
+        (let ((default-directory (locate-dominating-file
+                                  default-directory ".git"))
+              (val (ivy-read "pattern: " 'counsel-git-grep-function))
+              lst)
+          (when val
+            (setq lst (split-string val ":"))
+            (find-file (car lst))
+            (goto-char (point-min))
+            (forward-line (1- (string-to-number (cadr lst)))))))
+      (use-package ivy
+        :defer t
+        :config
+        (progn
+          (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
+          (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)))
+
+      (define-key global-map (kbd "C-s") 'swiper)
+      (setq ivy-use-virtual-buffers t)
+      (global-set-key (kbd "C-c C-r") 'ivy-resume)
+      (global-set-key (kbd "C-c j") 'counsel-git-grep))))
+
+
+
+(defun hejia/init-counsel ()
+  (use-package counsel
+    :init
+    (progn
+      (global-set-key (kbd "C-h v") 'counsel-describe-variable)
+      (global-set-key (kbd "C-h f") 'counsel-describe-function)
+      (evil-leader/set-key "hdv" 'counsel-describe-variable)
+      (evil-leader/set-key "hdf" 'counsel-describe-function)
+      (bind-key* "M-x" 'counsel-M-x)
+      (evil-leader/set-key dotspacemacs-command-key 'counsel-M-x)
+      )))
+
+
+(defun hejia/post-init-projectile ()
+  (use-package projectile
+    :defer t
+    :config
+    (progn
+      (add-to-list 'projectile-other-file-alist '("html" "js")) ;; switch from html -> js
+      (add-to-list 'projectile-other-file-alist '("js" "html")) ;; switch from js -> html
+      )))
+
+
+(defun hejia/post-init-find-file-in-project ()
+  (progn
+
+    ;; If you use other VCS (subversion, for example), enable the following option
+    ;;(setq ffip-project-file ".svn")
+    ;; in MacOS X, the search file command is CMD+p
+    (bind-key* "C-x p" 'find-file-in-project)
+    ;; for this project, I'm only interested certain types of files
+    ;; (setq-default ffip-patterns '("*.html" "*.js" "*.css" "*.java" "*.xml" "*.js"))
+    ;; if the full path of current file is under SUBPROJECT1 or SUBPROJECT2
+    ;; OR if I'm reading my personal issue track document,
+    (defadvice find-file-in-project (before my-find-file-in-project activate compile)
+      (when (ffip-current-full-filename-match-pattern-p "\\(/fireball\\)")
+        ;; set the root directory into "~/projs/PROJECT_DIR"
+        (setq-local ffip-project-root "~/Github/fireball")
+        ;; well, I'm not interested in concatenated BIG js file or file in dist/
+        (setq-local ffip-find-options "-not -size +64k -not -iwholename '*/bin/*'")
+        ;; do NOT search files in below directories, the default value is better.
+        ;; (setq-default ffip-prune-patterns '(".git" ".hg" "*.svn" "node_modules" "bower_components" "obj"))
+        )
+      (when (ffip-current-full-filename-match-pattern-p "\\(/cocos2d-x\\)")
+        ;; set the root directory into "~/projs/PROJECT_DIR"
+        (setq-local ffip-project-root "~/cocos2d-x")
+        ;; well, I'm not interested in concatenated BIG js file or file in dist/
+        (setq-local ffip-find-options "-not -size +64k -not -iwholename '*/bin/*'")
+        ;; do NOT search files in below directories, the default value is better.
+        ;; (setq-default ffip-prune-patterns '(".git" ".hg" "*.svn" "node_modules" "bower_components" "obj"))
+        ))
+    (ad-activate 'find-file-in-project)))
+
+
+(defun hejia/init-find-file-in-project ()
+  (use-package find-file-in-project
+    :defer t
+    :init))
+
+
+(defun hejia/init-helm-github-stars ()
+  (use-package helm-github-stars
+    :defer t
+    :config
+    (progn
+      (setq helm-github-stars-username "vikatory")
+      (setq helm-github-stars-cache-file "~/.emacs.d/.cache/hgs-cache"))))
+
+
+(defun hejia/post-init-magit ()
+  (use-package magit
+    :defer t
+    :config
+    (progn
+      (add-to-list 'magit-no-confirm 'stage-all-changes)
+      (define-key magit-log-mode-map (kbd "W") 'magit-copy-as-kill)
+      (define-key magit-status-mode-map (kbd "s-1") 'magit-jump-to-unstaged)
+      (define-key magit-status-mode-map (kbd "s-2") 'magit-jump-to-untracked)
+      (define-key magit-status-mode-map (kbd "s-3") 'magit-jump-to-staged)
+      (define-key magit-status-mode-map (kbd "s-4") 'magit-jump-to-stashes)
+
+      ;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+      ;; (add-hook 'magit-section-set-visibility-hook '(lambda (section) (let ((section-type (magit-section-type section)))
+      ;;                                                              (if (or (eq 'untracked section-type)
+      ;;                                                                      (eq 'stashes section-type))
+      ;;                                                                  'hide))))
+      )
+
+    :init
+    (progn
+      ;; Githu PR settings
+      ;; "http://endlessparentheses.com/create-github-prs-from-emacs-with-magit.html"
+      (setq magit-repository-directories '("~/cocos2d-x/"))
+      (setq magit-push-always-verify nil)
+
+
+      (defun endless/visit-pull-request-url ()
+        "Visit the current branch's PR on Github."
+        (interactive)
+        (browse-url
+         (format "https://github.com/%s/pull/new/%s"
+                 (replace-regexp-in-string
+                  "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+                  (magit-get "remote"
+                             (magit-get-remote)
+                             "url"))
+                 (cdr (magit-get-remote-branch)))))
+
+
+      (eval-after-load 'magit
+        '(define-key magit-mode-map (kbd "s-g")
+           #'endless/visit-pull-request-url))
+
+
+      (defadvice magit-blame-mode (after magit-blame-change-to-emacs-state activate compile)
+        "when entering magit blame mode, change evil normal state to emacs state"
+        (if (evil-normal-state-p)
+            (evil-emacs-state)
+          (evil-normal-state)))
+
+      (ad-activate 'magit-blame-mode)
+
+      (defadvice git-timemachine-mode (after git-timemachine-change-to-emacs-state activate compile)
+        "when entering git-timemachine mode, change evil normal state to emacs state"
+        (if (evil-normal-state-p)
+            (evil-emacs-state)
+          (evil-normal-state)))
+
+      (ad-activate 'git-timemachine-mode)
+
+      (setq magit-process-popup-time 10))))
+
+
+(defun hejia/post-init-git-messenger ()
+  (use-package git-messenger
+    :defer t
+    :config
+    (progn
+      (defun my-vc-visit-file-revision (file rev)
+        "Visit revision REV of FILE in another window.
+With prefix argument, uses the current window instead.
+If the current file is named `F', the revision is named `F.~REV~'.
+If `F.~REV~' already exists, use it instead of checking it out again."
+        ;; based on `vc-revision-other-window'.
+        (interactive
+         (let ((file (expand-file-name
+                      (read-file-name
+                       (if (buffer-file-name)
+                           (format "File (%s): " (file-name-nondirectory
+                                                  (buffer-file-name)))
+                         "File: ")))))
+           (require 'vc)
+           (unless (vc-backend file)
+             (error "File %s is not under version control" file))
+           (list file (vc-read-revision
+                       "Revision to visit (default is working revision): "
+                       (list file)))))
+        (require 'vc)
+        (unless (vc-backend file)
+          (error "File %s is not under version control" file))
+        (let ((revision (if (string-equal rev "")
+                            (vc-working-revision file)
+                          rev))
+              (visit (if current-prefix-arg
+                         'switch-to-buffer
+                       'switch-to-buffer-other-window)))
+          (funcall visit (vc-find-revision file revision))))
+
+      (define-key git-messenger-map (kbd "f") 'my-vc-visit-file-revision))))
+
+
+(defun hejia/init-helm-ls-git ()
+  (use-package helm-ls-git
+    :init
+    (progn
+      ;;beautify-helm buffer when long file name is present
+      (setq helm-ls-git-show-abs-or-relative 'relative))))
+
+
+(defun hejia/init-beacon ()
+  (use-package beacon
+    :init
+    (progn
+      (spacemacs|add-toggle beacon
+        :status beacon-mode
+        :on (beacon-mode)
+        :off (beacon-mode -1)
+        :documentation "Enable point highlighting after scrolling"
+        :evil-leader "otb")
+
+      (spacemacs/toggle-beacon-on))
+    :config (spacemacs|hide-lighter beacon-mode)))
+
+
+(defun hejia/init-evil-vimish-fold ()
+  (use-package evil-vimish-fold
+    :init
+    (vimish-fold-global-mode 1)))
+
+
+(defun hejia/init-impatient-mode ()
+  "Initialize impatient mode"
+  (use-package impatient-mode
+    :init
+    (progn
+
+      (defun hejia-mode-hook ()
+        "my web mode hook for HTML REPL"
+        (interactive)
+        (impatient-mode)
+        (httpd-start))
+
+      (add-hook 'web-mode-hook 'hejia-mode-hook)
+      (evil-leader/set-key-for-mode 'web-mode
+        "p" 'imp-visit-buffer)
+      )))
+
+
+;; 输入热键后，输入单词开头的字符，根据热键提示跳转
+(defun hejia/post-init-avy ()
+  (use-package avy
+    :defer t
+    :init
+    (progn
+      (global-set-key (kbd "C-'") 'avy-goto-char-2))))
+
+
+(defun hejia/init-multiple-cursors ()
+  (use-package multiple-cursors
+    :init
+    (progn
+      (bind-key* "C-S-c C-S-c" 'mc/edit-lines)
+      (bind-key* "C-c f" 'mc/mark-all-dwim)
+      (bind-key* "C->" 'mc/mark-next-like-this)
+      (bind-key* "C-<" 'mc/mark-previous-like-this)
+      (bind-key* "C-<f11>" 'mc/unmark-next-like-this)
+      (bind-key* "C-<f12>" 'mc/unmark-previous-like-this)
+      (bind-key* "C-c C-<" 'mc/mark-all-like-this)
+
+
+      ;; http://endlessparentheses.com/multiple-cursors-keybinds.html?source=rss
+      (define-prefix-command 'endless/mc-map)
+      ;; C-x m is usually `compose-mail'. Bind it to something
+      ;; else if you use this command.
+      (define-key ctl-x-map "m" 'endless/mc-map)
+      ;; Really really nice!
+      (define-key endless/mc-map "i" #'mc/insert-numbers)
+      (define-key endless/mc-map "h" #'mc-hide-unmatched-lines-mode)
+      (define-key endless/mc-map "a" #'mc/mark-all-like-this)
+
+      ;; Occasionally useful
+      (define-key endless/mc-map "d" #'mc/mark-all-symbols-like-this-in-defun)
+      (define-key endless/mc-map "r" #'mc/reverse-regions)
+      (define-key endless/mc-map "s" #'mc/sort-regions)
+      (define-key endless/mc-map "l" #'mc/edit-lines)
+      (define-key endless/mc-map "\C-a" #'mc/edit-beginnings-of-lines)
+      (define-key endless/mc-map "\C-e" #'mc/edit-ends-of-lines)
+      )))
+
+
+
+
+
+
+
+
+
+
+
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
 ;; https://github.com/jwiegley/use-package
